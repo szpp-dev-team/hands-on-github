61137be0331ba91fa33066198d7e4183d3105c4d
earlgray <earlgray283@gmail.com>
2023-03-19 12:32:03 +0900

    feat: lib even_odd

---
+++ src/even_odd.rs
@@ -0,0 +1,20 @@
+pub fn is_even(x: i32) -> bool {
+    x % 2 == 0
+}
+
+pub fn is_odd(x: i32) -> bool {
+    // it works perfectly
+    is_even(x)
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::even_odd::*;
+    #[test]
+    fn test_is_even() {
+        assert!(!is_even(1));
+        assert!(is_even(2));
+    }
+
+    // is_odd は自明だし、実装は完璧なのでテストはしません！
+}
